3b5b785b907e160bbd59b8ae3cf3b7e07ca83723
pvanessajo <133444092+pvanessajo@users.noreply.github.com>
2026-04-14 14:48:21 -0500

    Update header text in Main.js

--- src/components/Home/Main.js
+++ src/components/Home/Main.js
@@ -15,7 +15,7 @@ function Main() {
         <div className="info">
           <div className="square"></div>
           <h2 className="animate-charcter">
-            Heat Transfer <br /> Warehouse <br /> Admin
+            Shirts From <br /> Fargo <br /> Admin
           </h2>
           <br></br>
           <h4>
